b5f4fd1074855c4a1be272a15963a87229472c9f
parrnavi19 <parnavi.l@somaiya.edu>
2026-03-23 23:17:58 +0530

    Update register.php

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/header.php';
 if (isset($_SESSION['user_id'])) { header("Location: index.php"); exit; }
 
 $error = '';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+{
   $username = trim($_POST['username'] ?? '');
   $email    = trim($_POST['email'] ?? '');
   $password = $_POST['password'] ?? '';
